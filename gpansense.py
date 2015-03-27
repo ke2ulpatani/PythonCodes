bd89d6860fe6bac78516cc095dbd92394537e6e9
Ketul Patani <ke2ulpatani>
2015-03-27 21:49:03 +0530

    SRM Gpa Calculator

--- gpansense.py
+++ gpansense.py
@@ -1,5 +1,6 @@
 import os
 import urllib2 as ul
+#import MySQLdb
 from bs4 import BeautifulSoup as bs
 
 grades = {"A+":10,"A":9.5,"A-":9,"B+":8.5,"B":8,"B-":7.5,"C+":7,"C":6.5,"C-":6,"D+":5.5,"D":5,"D-":4.5,"E":4}
