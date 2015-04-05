d1badc613f2eb937c36d4cf8e2ea8ec3c4d44e94
Ketul Patani <ke2ulpatani>
2015-04-05 14:13:34 +0530

    Program which displays the used virtual memory percentage

---
+++ check_ram.py
@@ -0,0 +1,20 @@
+import psutil
+
+def find_between_r( s, first, last ):
+    try:
+        start = s.rindex( first ) + len( first )
+        end = s.rindex( last, start )
+        return s[start:end]
+    except ValueError:
+        return ""
+
+if __name__ == "__main__":
+    s = psutil.virtual_memory()
+    s = str(s)
+    s = find_between_r(s,"percent=","used").strip().replace(",","")
+    s = float(s)
+    print s
+    if s > 95.0:
+        print 'Critical'
+    else:
+        print 'Cool'
